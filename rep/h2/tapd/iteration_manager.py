8b90af794414eb0e04df4bc0b65c2e0d522c120a
zhaojiaxing <zhaojiaxing@dailyyoga.com>
2023-03-14 11:43:17 +0800

    逻辑调整

--- rep/h2/tapd/iteration_manager.py
+++ rep/h2/tapd/iteration_manager.py
@@ -13,11 +13,11 @@ def get_iteration_our(people, days):
   π：5人
 '''
 dev_owner = {'王奕娇;', '王媛;', '高强;', '侯维维;', '林军;', '贾宇光;', '张伟;', '高哲;', '徐培帅;', '曹永胜;',
-             '高祥;',
-             '秦浩楠;', '万雪婷;', '李正祥;', '王成斌;', '苏帅龙;', '吴华友;', '徐培帅;', '乔天良;', '马廉;', '张梦佳;'}
+             '高祥;', '秦浩楠;', '万雪婷;', '李正祥;', '王成斌;', '苏帅龙;', '吴华友;', '徐培帅;', '乔天良;', '马廉;',
+             '张梦佳;'}
 
 
-def get_tasks():
+def get_tasks(num):
     dev_end = {}
     for name in dev_owner:
         task_arr = tapd_parse.get_tasks(name)
@@ -29,8 +29,9 @@ def get_tasks():
             num_begin = int(str(begin).replace('-', ''))
             if max_end < num_begin:
                 max_end = num_begin
-        dev_end[name] = max_end
-    print(dev_end)
+        if max_end < num:
+            dev_end[name] = max_end
+            print(name, max_end)
 
 
-get_tasks()
+get_tasks(20230316)
