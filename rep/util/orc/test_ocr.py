bcfda7c286473f0bd5bcbec748887534b2c957ba
zhaojiaxing <zhaojiaxing@dailyyoga.com>
2023-05-31 09:44:03 +0800

    增加图片ocr提取识别

--- rep/util/orc/test_ocr.py
+++ rep/util/orc/test_ocr.py
@@ -5,26 +5,25 @@ reader = easyocr.Reader(['ch_sim', 'en'])
 order_images = ["order.jpg", "order_2.jpg", "order_3.jpg", "order_4.jpg"]
 
 for image in order_images:
-    print(image)
     result = reader.readtext(image, detail=0)
-    print(result)
     for i in range(0, len(result)):
-        order_id = ""
-        if result[i] == "商家订单号" or result[i] == "商户单号":
-            order_id = result[i + 1]
-        if "订单编号: " in result[i]:
-            order_id = result[i].replace("订单编号: ", "")
-        elif "订单编号" in result[i]:
-            order_id = result[i + 1]
+        print(result[i])
+        # order_id = ""
+        # if result[i] == "商家订单号" or result[i] == "商户单号":
+        #     order_id = result[i + 1]
+        # if "订单编号: " in result[i]:
+        #     order_id = result[i].replace("订单编号: ", "")
+        # elif "订单编号" in result[i]:
+        #     order_id = result[i + 1]
+        #
+        # if order_id == "":
+        #     continue
+        #
+        # if "0P" in order_id:
+        #     order_id = order_id.replace("0P", "OP")
+        # if " " in order_id:
+        #     order_id = order_id.replace(" ", "")
 
-        if order_id == "":
-            continue
-
-        if "0P" in order_id:
-            order_id = order_id.replace("0P", "OP")
-        if " " in order_id:
-            order_id = order_id.replace(" ", "")
-
-        print(image, order_id)
+        # print(image, order_id)
 
 # print(reader.readtext("order.jpg", detail=0))
